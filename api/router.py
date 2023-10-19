790d6dab2fe523040fc5f6aac0a36c68f28dbe06
Rafael Athilla <rafael.athilla@ativossa.com.br>
2023-10-19 20:55:57 -0300

    .

--- api/router.py
+++ api/router.py
@@ -3,4 +3,4 @@ from fastapi import APIRouter
 from api.v1.endpoints import predict
 
 api_router = APIRouter()
-api_router.include_router(predict.router, prefix="/Iris", tags=['Predict Iris'])
+api_router.include_router(predict.router, prefix="/iris", tags=["Predict Iris"])
